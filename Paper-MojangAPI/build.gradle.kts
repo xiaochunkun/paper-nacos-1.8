a69a253f2f37cea483d71119da1e6a625170fbd8
xiaochunkun <38680491+xiaochunkun@users.noreply.github.com>
2023-11-23 15:31:57 +0800

    Initialize api-capabilities

---
+++ Paper-MojangAPI/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+    maven("https://libraries.minecraft.net")
+}
+
+dependencies {
+    implementation(project(":paper-api"))
+    api("com.mojang:brigadier:1.0.18")
+
+    compileOnly("it.unimi.dsi:fastutil:8.2.2")
+    compileOnly("org.jetbrains:annotations:18.0.0")
+
+    testImplementation("junit:junit:4.13.1")
+    testImplementation("org.hamcrest:hamcrest-library:1.3")
+    testImplementation("org.ow2.asm:asm-tree:7.3.1")
+}
+
+configurations {
+    val outgoing = arrayOf(runtimeElements, apiElements, named("sourcesElements"), named("javadocElements"))
+    for (config in outgoing) {
+        config {
+            outgoing {
+                capability("${project.group}:${project.name}:${project.version}")
+                capability("com.destroystokyo.paper:paper-mojangapi:${project.version}")
+            }
+        }
+    }
+}
+
+configure<PublishingExtension> {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
